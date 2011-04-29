93d6c5ae41c14e544d330daa93920adb60c86f8a
Rüdiger Marwein <henryxiv@hotmail.com>
2011-04-29 16:53:13 +0200

    Added annotations to Helper class

--- classes/Helpers.php
+++ classes/Helpers.php
@@ -6,6 +6,9 @@ class Helpers {
 	
 	/**
 	 * Encode destructive contents in HTML
+	 * 
+	 * @param string Text to encode
+	 * @return string htmlspecialchar'd string
 	 */
 	static function h($string) {
 		return htmlspecialchars($string);
@@ -13,6 +16,9 @@ class Helpers {
 	
 	/**
 	 * Encode destructive content in URL
+	 * 
+	 * @param string Text to encode
+	 * @return string rawurlencoded string
 	 */
 	static function u($string) {
 		return rawurlencode($string);
@@ -20,6 +26,12 @@ class Helpers {
 	
 	/**
 	 * Create a valid link within Framework
+	 * 
+	 * @param string Module
+	 * @param string Controller
+	 * @param string Action
+	 * @param string GET Paramaters without "?"
+	 * @return string Link to Module/Controller/Action?Param
 	 */
 	static function link($module,$controller,$action,$param='') {
 		if($param) {
@@ -29,7 +41,14 @@ class Helpers {
 	}
 	
 	/**
-	 * Redirect to another page.
+	 * Redirect to another page. 
+	 * 
+	 * Adds HTTP header and stops script execution.
+	 * 
+	 * @param string Module
+	 * @param string Controller
+	 * @param string Action
+	 * @param string GET Paramaters without "?"
 	 * @see link()
 	 */
 	static function redirect($module, $controller,$action,$param='') {
