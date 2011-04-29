0435e03a0f2551e7cc1e7c7acc62e94e79b36eca
Rüdiger Marwein <henryxiv@hotmail.com>
2011-04-29 17:09:45 +0200

    Added comments to MySQL class

--- classes/MySQL.php
+++ classes/MySQL.php
@@ -1,13 +1,43 @@
 <?php
+/**
+ * MySQLi simplification class
+ */
 class MySQL {
+	
+	/**
+	 * @var resource Database connection resource object
+	 */
 	protected static $resource = null;
 	
+	/**
+	 * Connect to the database
+	 * 
+	 * @param string Hostname
+	 * @param string Username
+	 * @param string Password
+	 * @param string Database name
+	 * @return boolean true on success
+	 */
 	static function connect($host,$user,$pass,$db) {
 		$db = new mysqli($host,$user,$pass,$db);
 		self::$resource = $db;
-		// TODO: error handling
+		return self::$resource !== null;
 	}
 	
+	/**
+	 * Query the database (perform a select statement)
+	 * 
+	 * Mimics the behaviour of binding parameters for simpler use.
+	 * 
+	 * Example:
+	 * $rows = $db->query("SELECT * FROM users where id=? and email=?",
+	 * 	 array(5, 'test@example.com'));
+	 * 
+	 * @param string SQL statement. Must be a select or similar.
+	 * @param array Bind values. One for each "?"
+	 * @param string Class name of the result objects.
+	 * @return array of objects
+	 */
 	static function query($sql,$values=array(),$className=null) {
 		foreach($values as $k=>$v) {
 			$sql = preg_replace('/\?/', "'".self::$resource->real_escape_string($v)."'", $sql, 1);
@@ -26,6 +56,15 @@ class MySQL {
 		return $rows;
 	}
 	
+	/**
+	 * Execute a command (other then select, e.g. DELETE OR UPDATE)
+	 * 
+	 * @param string SQL statement. Must be a select or similar.
+	 * @param array Bind values. One for each "?"
+	 * @param string Charcter list of types. E.g. "ssds"
+	 * @see prepareCommand()
+	 * @return integer Affected rows
+	 */
 	static function execute($sql,$values,$types=null) {
 		$stmt = self::prepareCommand($sql,$values,$types);
 		if(strpos(strtolower(trim($sql)),'insert')===0) {
@@ -34,19 +73,33 @@ class MySQL {
 		return $stmt->affected_rows;
 	}
 	
+	/**
+	 * Prepare a command (other then select, e.g. DELETE OR UPDATE)
+	 * 
+	 * @throws Exception if Statement could not be prepared.
+	 * @param string SQL statement. Must be a select or similar.
+	 * @param array Bind values. One for each "?"
+	 * @param string Character list of types. E.g. "ssds"
+	 * @return object MySQLi Statement object, null on failure
+	 */
 	static function prepareCommand($sql,$values=array(),$types=null) {
 		if(self::$resource) {
 			$stmt = self::$resource->prepare($sql);
 			if($stmt) {
+				/**
+				 * Use sequence of "s" (String) if no type list given.
+				 */
 				if($types === null) {
 					$types = str_repeat('s',count($values)); 
 				}
+				/**
+				 * Bind parameters
+				 */
 				if(strlen($types)) {
 				  $args = $values;
 				  array_unshift($args,$types);
 				  call_user_method_array("bind_param", $stmt, $args);
-				}
-				
+				}		
 			} else {
 				throw new Exception('MYSQL: Statement could not be created');
 			}
