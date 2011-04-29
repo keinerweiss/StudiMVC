17cd9b3a81a4917d422e0b9797d52f6d9b03841f
Rüdiger Marwein <henryxiv@hotmail.com>
2011-04-29 16:40:04 +0200

    Added comments to Helper class

--- classes/Helpers.php
+++ classes/Helpers.php
@@ -1,13 +1,26 @@
 <?php
+/**
+ * Helpers for common tasks
+ */
 class Helpers {
+	
+	/**
+	 * Encode destructive contents in HTML
+	 */
 	static function h($string) {
 		return htmlspecialchars($string);
 	}
 	
+	/**
+	 * Encode destructive content in URL
+	 */
 	static function u($string) {
 		return rawurlencode($string);
 	}
 	
+	/**
+	 * Create a valid link within Framework
+	 */
 	static function link($module,$controller,$action,$param='') {
 		if($param) {
 			return Config::get('relDir')."/index.php/$module/$controller/$action?$param";
@@ -15,6 +28,10 @@ class Helpers {
 		return Config::get('relDir')."/index.php/$module/$controller/$action";
 	}
 	
+	/**
+	 * Redirect to another page.
+	 * @see link()
+	 */
 	static function redirect($module, $controller,$action,$param='') {
 		header('Location: '.self::link($module,$controller,$action,$param));
 		exit;
@@ -22,5 +39,10 @@ class Helpers {
 
 }
 
+/**
+ * Shortcut for easier use.
+ * E.g. 
+ * echo H::link('admin','users','get');
+ */
 class H extends Helpers {
 }
