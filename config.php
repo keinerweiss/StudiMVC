14cb4533cfcdec2ec754556327c6367dac0aeb37
keinerweiss <ruediger.marwein@googlemail.com>
2011-05-04 22:14:49 +0200

    changed defaul settings of db and fixed filesystem problem on windows systems

--- config.php
+++ config.php
@@ -4,19 +4,19 @@ require_once('classes/Config.php');
 require_once('classes/MySQL.php');
 require_once('classes/Controller.php');
 
-$dirFS = dirname(__FILE__);
+// use $dirFS = '/path/to/here'; if paths are not correct
+$dirFS = str_replace("\\",'/',dirname(__FILE__));
 $dirWeb = str_replace($_SERVER['DOCUMENT_ROOT'],'',$dirFS);
 
 Config::init(array(
 	'projectDir' => $dirFS,
 	'relDir' => $dirWeb,
-    
 	'db' => array(
-		'enabled' => false,
+		'enabled' => true,
 		'host' => 'localhost',
 		'user' =>  'root',
 		'password' => '',
-		'database' => 'project_db'
+		'database' => 'database_name'
 	),	
 	'modules' => array(
 		'module' => array(),
